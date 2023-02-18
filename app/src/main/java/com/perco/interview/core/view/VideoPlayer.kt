02e9167725f1fe9e1ef0888b7dc5e8a393010b96
hananrofehaim <hanan.rofe.haim@gmail.com>
2023-02-18 13:40:36 +0200

    Added lifecycle awareness to player

--- app/src/main/java/com/perco/interview/core/view/VideoPlayer.kt
+++ app/src/main/java/com/perco/interview/core/view/VideoPlayer.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -44,9 +47,13 @@ fun VideoPlayer(
         if (play) {
             exoPlayer.play()
         } else {
-            exoPlayer.pause()
+            if (exoPlayer.isPlaying) {
+                exoPlayer.pause()
+            }
         }
     }
+
+    LifecycleEffect(exoPlayer, play)
 
     DisposableEffect(
         AndroidView(
@@ -61,6 +68,34 @@ fun VideoPlayer(
     ) {
         onDispose {
             exoPlayer.release()
+        }
+    }
+}
+
+@Composable
+private fun LifecycleEffect(
+    exoPlayer: ExoPlayer,
+    play: Boolean,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(play) {
+        val observer = object : DefaultLifecycleObserver {
+            override fun onPause(owner: LifecycleOwner) {
+                if (exoPlayer.isPlaying) {
+                    exoPlayer.pause()
+                }
+            }
+
+            override fun onResume(owner: LifecycleOwner) {
+                if (play) {
+                    exoPlayer.play()
+                }
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
 }
