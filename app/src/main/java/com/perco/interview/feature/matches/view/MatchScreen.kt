635359e2b72e9b2b16f9e65f1860349728da97ac
hananrofehaim <hanan.rofe.haim@gmail.com>
2023-02-18 12:15:21 +0200

    Integrated horizontal pager

--- app/src/main/java/com/perco/interview/feature/matches/view/MatchScreen.kt
+++ app/src/main/java/com/perco/interview/feature/matches/view/MatchScreen.kt
@@ -1,11 +1,8 @@
 package com.perco.interview.feature.matches.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,9 +10,12 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
 import org.koin.androidx.compose.getViewModel
 import org.koin.core.parameter.parametersOf
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MatchScreen(
     matchId: String,
@@ -23,16 +23,14 @@ fun MatchScreen(
 ) {
     val match by viewModel.state.collectAsState()
     if (match.isSuccess) {
-        LazyColumn(
+        val videos = match.getOrThrow()
+        HorizontalPager(
             modifier = Modifier
                 .fillMaxSize()
                 .background(MaterialTheme.colors.background)
-                .padding(10.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-            items(match.getOrThrow()) {
-                Text(text = it)
-            }
+                .padding(10.dp), count = videos.size
+        ) { page ->
+            Text(text = videos[page])
         }
     } else {
         Text(text = "Failed to load: ${match.exceptionOrNull()}")
